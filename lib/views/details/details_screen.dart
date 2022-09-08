7f9d9e442ef4dec75ad92358070889ef583bfa3a
Mehmet Doğru <mdogru685@gmail.com>
2022-09-08 16:04:53 +0300

    Details Page added

---
+++ lib/views/details/details_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_nodejs_restapi/contollers/product/product_controller.dart';
+import 'package:flutter_nodejs_restapi/helpers/textstyle_helper.dart';
+import 'package:get/get.dart';
+
+class DetailsPage extends StatelessWidget {
+  int index;
+
+  DetailsPage({Key? key, required this.index}) : super(key: key);
+
+  final ProductController _controller = Get.put(ProductController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_controller.productList[index].name),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: Get.width * 0.9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Hero(
+                tag: index,
+                child: Center(
+                  child: _controller.isLoading.value
+                      ? Image.network(
+                          _controller.productList[index].image.toString(),
+                        )
+                      : const CircularProgressIndicator(),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(Get.width * 0.05),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Get.width * 0.02),
+                  color: Colors.blue,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Fiyat : " + _controller.productList[index].price.toString() + " TL",
+                      style: TextStyleHelper.titleStyle,
+                    ),
+                    Text(
+                      "Açıklama : " + _controller.productList[index].description,
+                      style: TextStyleHelper.titleStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nodejs_restapi/contollers/product/product_controller.dart';
 import 'package:flutter_nodejs_restapi/helpers/textstyle_helper.dart';
+import 'package:flutter_nodejs_restapi/views/details/details_screen.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
@@ -21,7 +22,7 @@ class HomePage extends StatelessWidget {
                 shrinkWrap: true,
                 itemCount: _controller.productList.length,
                 itemBuilder: (context, index) {
-                  return _productCard(index);
+                  return _productCard(index, context);
                 },
               )
             : const Center(child: CircularProgressIndicator()),
@@ -29,66 +30,83 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Container _productCard(int index) {
-    return Container(
-      margin: EdgeInsets.all(Get.width * 0.02),
-      width: Get.width * 0.8,
-      decoration: BoxDecoration(
-        color: Colors.teal,
-        borderRadius: BorderRadius.circular(Get.width * 0.05),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(Get.width * 0.04),
-        child: Column(
-          children: [
-            _controller.productList[index].image == null
-                ? const Placeholder()
-                : ClipRRect(
-                    borderRadius: BorderRadius.circular(Get.width * 0.05),
-                    child: Image.network(
-                      _controller.productList[index].image!,
-                      fit: BoxFit.cover,
-                      scale: 4,
+  InkWell _productCard(int index, BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsPage(index: index),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(Get.width * 0.02),
+        width: Get.width * 0.8,
+        decoration: BoxDecoration(
+          color: Colors.teal,
+          borderRadius: BorderRadius.circular(Get.width * 0.05),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(Get.width * 0.04),
+          child: Column(
+            children: [
+              _controller.productList[index].image == null
+                  ? const Placeholder()
+                  : Hero(
+                      tag: index,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(Get.width * 0.05),
+                        child: Image.network(
+                          _controller.productList[index].image!,
+                          fit: BoxFit.cover,
+                          scale: 4,
+                        ),
+                      ),
+                    ),
+              SizedBox(height: Get.width * 0.03),
+              Row(
+                children: [
+                  Text(
+                    "Ürün Adı : ",
+                    style: TextStyleHelper.titleStyle,
+                  ),
+                  Text(
+                    _controller.productList[index].name,
+                    style: TextStyleHelper.subtitleStyle,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Ürün Açıklaması : ",
+                    style: TextStyleHelper.titleStyle,
+                  ),
+                  Expanded(
+                    child: Text(
+                      _controller.productList[index].description,
+                      style: TextStyleHelper.subtitleStyle,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
                     ),
                   ),
-            SizedBox(height: Get.width * 0.03),
-            Row(
-              children: [
-                Text(
-                  "Ürün Adı : ",
-                  style: TextStyleHelper.titleStyle,
-                ),
-                Text(
-                  _controller.productList[index].name,
-                  style: TextStyleHelper.subtitleStyle,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Ürün Açıklaması : ",
-                  style: TextStyleHelper.titleStyle,
-                ),
-                Text(
-                  _controller.productList[index].description,
-                  style: TextStyleHelper.subtitleStyle,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Fiyat : ",
-                  style: TextStyleHelper.titleStyle,
-                ),
-                Text(
-                  _controller.productList[index].price.toString() + " TL",
-                  style: TextStyleHelper.subtitleStyle,
-                ),
-              ],
-            )
-          ],
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Fiyat : ",
+                    style: TextStyleHelper.titleStyle,
+                  ),
+                  Text(
+                    _controller.productList[index].price.toString() + " TL",
+                    style: TextStyleHelper.subtitleStyle,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
